69b4574924566508a2fa57c19f0b90092cd3879d
Sora Shigemitsu <sora11023539@gmail.com>
2023-08-05 13:30:47 +0900

    fix: source変更

--- backend/routes/books.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-const router = express.Router();
-
-/* GET books listing */
-router.get('/books', async (req: Request, res: Response, next: any) => {
-  res.json({ title: 'I am a cat.' });
-});
-
-export default router;
