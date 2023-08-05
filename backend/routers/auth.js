a06b412ca763c41ba7d876421e90b6eb1fa175ba
Sora Shigemitsu <sora11023539@gmail.com>
2023-08-05 16:23:42 +0900

    refactor: auth

---
+++ backend/routers/auth.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const prisma = new PrismaClient();
+
+// create user
+router.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return res.json({ user });
+});
+
+// login
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    return res.status(401).json({ error: 'そのユーザーは存在しません' });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: 'そのパスワードは間違っています' });
+  }
+
+  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+    expiresIn: '1d',
+  });
+
+  return res.json({ token });
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,12 @@
 const express = require('express');
 const app = express();
-const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-
 const PORT = 8080;
-const prisma = new PrismaClient();
+const authRoutes = require('./routers/auth')
+
+require('dotenv').config();
 
 app.use(express.json());
 
-// create user
-app.post('/api/auth/register', async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  return res.json({ user });
-});
-
-// login
-app.post('/api/auth/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await prisma.user.findUnique({ where: { email } });
-
-  if (!user) {
-    return res.status(401).json({ error: 'そのユーザーは存在しません' });
-  }
-
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-
-  if (!isPasswordValid) {
-    return res.status(401).json({ error: 'そのパスワードは間違っています' });
-  }
-
-  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
-    expiresIn: '1d',
-  });
-
-  return res.json({ token });
-});
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT);
