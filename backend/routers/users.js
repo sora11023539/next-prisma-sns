177b69d364cc813659c3cd97a30a17adc51af06a
sora11023539 <sora11023539@gmail.com>
2023-08-08 01:39:28 +0900

    add: get user

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -20,4 +20,30 @@ router.get('/find', isAuthenticated, async (req, res) => {
   }
 });
 
+router.get('/profile/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const profile = await prisma.profile.findUnique({
+      where: { userId: parseInt(userId) },
+      include: {
+        user: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+
+    if (!profile) {
+      return res.status(404).json({ message: 'プロフィールが見つかりませんでした' });
+    }
+
+    res.status(200).json(profile);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error:error.message})
+  }
+});
+
 module.exports = router;
---
+++ frontend/pages/profile/[userId].tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { GetServerSideProps } from "next";
+import apiClient from '@/lib/apiClient';
+import { ProfileType } from '@/types';
+
+type Props = {
+  profile: ProfileType;
+}
+
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+  const { userId } = context.query;
+
+  try {
+    const profileResponse = await apiClient.get(`/users/profile/${userId}`)
+
+    return {
+      props: {
+        profile: profileResponse.data,
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      notFound: true,
+    }
+  }
+};
+
+const UserProfile = ({ profile }: Props) => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="w-full max-w-xl mx-auto">
+        <div className="bg-white shadow-md rounded-lg p-6 mb-4">
+          <div className="flex items-center">
+            <img
+              className="w-20 h-20 rounded-full mr-4"
+              alt="User Avatar"
+              src={profile.profileImageUrl}
+            />
+            <div>
+              <h2 className="text-2xl font-semibold mb-1">
+                {profile.user.username}
+              </h2>
+              <p className="text-gray-600">
+                {profile.bio}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white shadow-md rounded p-4 mb-4">
+          <div className="mb-4">
+            <div className="flex items-center mb-2">
+              <img
+                className="w-10 h-10 rounded-full mr-2"
+                alt="User Avatar"
+              />
+              <div>
+                <h2 className="font-semibold text-md"></h2>
+                <p className="text-gray-500 text-sm">
+                </p>
+              </div>
+            </div>
+            <p className="text-gray-700"></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UserProfile
