592fa14b923d2fe017ef492c78d0b8e77833cd10
sora11023539 <sora11023539@gmail.com>
2023-08-07 23:43:03 +0900

    add: get user

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080;
 const authRoutes = require('./routers/auth');
 const postsRoutes = require('./routers/posts');
+const usersRoutes = require('./routers/users');
 const cors = require('cors');
 
 require('dotenv').config();
@@ -12,5 +13,6 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api', postsRoutes);
+app.use('/api/users', usersRoutes);
 
 app.listen(PORT);
--- frontend/context/auth.tsx
+++ frontend/context/auth.tsx
@@ -1,7 +1,12 @@
 import apiClient from "@/lib/apiClient";
-import React, { ReactNode, useContext, useEffect } from "react";
+import React, { ReactNode, useContext, useEffect, useState } from "react";
 
 interface AuthContextType {
+  user: null | {
+    id: number,
+    username: string,
+    email: string,
+  };
   login: (token: string) => void;
   logout: () => void;
 }
@@ -11,6 +16,7 @@ interface AuthProviderProps {
 }
 
 const AuthContext = React.createContext<AuthContextType>({
+  user: null,
   login: () => { },
   logout: () => { },
 })
@@ -20,13 +26,31 @@ export const useAuth = () => {
 }
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<null | { id: number, email: string, username: string }>(null)
   useEffect(() => {
     const token = localStorage.getItem('auth_token');
-    apiClient.defaults.headers['Authorization'] = `Bearer ${token}`;
+    if (token) {
+      apiClient.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+      apiClient
+        .get('/users/find')
+        .then((res: any) => {
+          setUser(res.data.user);
+        })
+        .catch((error: any) => {
+          console.log(error);
+        })
+    }
   }, [])
 
   const login = async (token: string) => {
     localStorage.setItem('auth_token', token)
+
+    try {
+      apiClient.get('/users/find').then((res: any) => { setUser(res.data.user); })
+    } catch (error) {
+
+    }
   }
 
   const logout = () => {
@@ -34,6 +58,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   }
 
   const value = {
+    user,
     login,
     logout
   }
